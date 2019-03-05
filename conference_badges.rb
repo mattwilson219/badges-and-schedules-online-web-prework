8cce9a9cdb22a782b06c346c4ab28322c737cac6
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-05 21:28:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  batch_badge_creator(attendees).each do |badge|
+  assign_rooms(attendees).each do |badge|
     puts badge
   end
 end 
